a8df2cbb34826f5f79debaa9dcdf767c939f0e06
dudo48 <test>
2023-02-17 21:05:24 +0200

    added get average function

--- main.cpp
+++ main.cpp
@@ -12,8 +12,19 @@ int getSum(std::vector<int> vec)
     return sum;
 }
 
+double getAverage(std::vector<int> vec)
+{
+    double sum = 0;
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        sum += vec[i];
+    }
+    return sum / vec.size();
+}
+
 int main()
 {
     std::vector<int> vec {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::cout << getSum(vec) << '\n';
+    std::cout << getAverage(vec) << '\n';
 }
